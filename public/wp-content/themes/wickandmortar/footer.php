b5aa53f6ec1039c99cae3fc71abce0eb020dcdf4
mahmud <mahmud.linked@gmail.com>
2019-11-28 23:31:12 +0600

    Footer edited

--- public/wp-content/themes/wickandmortar/footer.php
+++ public/wp-content/themes/wickandmortar/footer.php
@@ -2,14 +2,14 @@
     <footer class="footer">
         <div class="container">
             <div class="row">
-                <div class="col-md-3">
+                <div class="col-md-4">
 
                     <?php dynamic_sidebar('footer_col_1'); ?>
 
                 </div>
 
-                <div class="col-md-6 text-center">
-                    <div class="footer-image-gellary">
+                <div class="col-md-4">
+                    <!-- <div class="footer-image-gellary">
                         <img src="<?php echo get_template_directory_uri(); ?>/assets/images/1.jpg" alt="">
                         <img src="<?php echo get_template_directory_uri(); ?>/assets/images/2.jpg" alt="">
                         <img src="<?php echo get_template_directory_uri(); ?>/assets/images/3.jpg" alt="">
@@ -18,19 +18,22 @@
                         <img src="<?php echo get_template_directory_uri(); ?>/assets/images/6.jpg" alt="">
                     </div>
                     <br>
-                    <a href="https://www.instagram.com/shopkantoka/"> See More </a>
+                    <a href="https://www.instagram.com/shopkantoka/"> See More </a> -->
+                    <?php dynamic_sidebar('footer_col_2'); ?>
                 </div>
 
-                <div class="col-md-3">
-                    <!-- <div class="social-icon">
+                <div class="col-md-4">
+                    <!-- <p style="padding-left:15%">
+                        <a href="mailto:info@kantoka.com">info@kantoka.com</a>
+                    </p>
+                    <div class="social-icon">
                         <ul>
-                            <li><a href="https://www.instagram.com/shopkantoka/"><span class="iconify" data-icon="fa:instagram" data-inline="false"></span></a></li>
                             <li><a href="https://www.twitter.com/shopkantoka/"><span class="iconify" data-icon="fa:twitter" data-inline="false"></span></a></li>
+                            <li><a href="https://www.instagram.com/shopkantoka/"><span class="iconify" data-icon="fa:instagram" data-inline="false"></span></a></li>
+                            <li><a href="https://www.twitter.com/shopkantoka/"><span class="iconify" data-icon="fa:linkedin" data-inline="false"></span></a></li>
                         </ul>
-                    </div>
-                    <p>
-                        <a href="mailto:info@kantoka.com">welcome@kantoka.com</a>
-                    </p> -->
+                    </div> -->
+                    
                     <?php dynamic_sidebar('footer_col_3'); ?>
                 </div>
             </div>
