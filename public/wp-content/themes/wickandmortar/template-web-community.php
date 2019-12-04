a1e84e4b96502f86bd99f17a556daaa6bda18ff8
mahmud <mahmud.linked@gmail.com>
2019-12-05 03:48:12 +0600

    web community fix

--- public/wp-content/themes/wickandmortar/template-web-community.php
+++ public/wp-content/themes/wickandmortar/template-web-community.php
@@ -31,14 +31,14 @@ get_header(); ?>
 				<a href="#">Connect with us ></a>
 			</div>
 
-			<div class="col-md-6 img_col order-1 order-md-2" style="background-image:url('http://kantoka.me/wp-content/themes/wickandmortar/assets/images/forefront.jpg');">
+			<div class="col-md-6 img_col order-1 order-md-2" style="background:url('http://kantoka.me/wp-content/themes/wickandmortar/assets/images/forefront.jpg');">
 			</div>
 
 		</div>
 
 		<div class="row">
 
-			<div class="col-md-6 img_col" style="background-image:url('http://kantoka.me/wp-content/themes/wickandmortar/assets/images/transform.jpg');">
+			<div class="col-md-6 img_col" style="background:url('http://kantoka.me/wp-content/themes/wickandmortar/assets/images/transform.jpg');">
 			</div>
 
 			<div class="col-md-6 text_col my-auto">
@@ -63,7 +63,7 @@ get_header(); ?>
 				<a href="#">Submit your idea here ></a>
 			</div>
 
-			<div class="col-md-6 img_col order-1 order-md-2" style="background-image:url('http://kantoka.me/wp-content/themes/wickandmortar/assets/images/forward.jpg');">
+			<div class="col-md-6 img_col order-1 order-md-2" style="background:url('http://kantoka.me/wp-content/themes/wickandmortar/assets/images/forward.jpg');">
 			</div>
 
 		</div>
